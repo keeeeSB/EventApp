0eafd0edcee26cb68f7d3955d0b48b52900562fc
KeeeeSB <kfp.a@icloud.com>
2025-11-08 08:56:29 +0900

    管理者用ルートページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+  }
+
+  namespace :admins do
+    root 'static_pages#dashboard'
+  end
+
   root 'static_pages#home'
 
   get 'up' => 'rails/health#show', as: :rails_health_check
