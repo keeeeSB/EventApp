0c5aa8e44da0a00f2e559f5e167a6446c5c5b813
KeeeeSB <kfp.a@icloud.com>
2025-11-08 13:15:30 +0900

    テストの追加

---
+++ spec/system/admins/sign_ins_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe '管理者ログイン機能', type: :system do
+  let!(:admin) { create(:admin, email: 'admin@example.com', password: 'password12345') }
+
+  describe '管理者ログイン' do
+    it '管理者は管理画面にログインできる' do
+      visit new_admin_session_path
+
+      fill_in 'メールアドレス', with: 'admin@example.com'
+      fill_in 'パスワード', with: 'password12345'
+      click_button 'ログインする'
+
+      expect(page).to have_content 'ログインしました。'
+      expect(page).to have_current_path admins_root_path
+
+      expect(page).to have_selector 'h2', text: 'ダッシュボード'
+    end
+  end
+
+  describe '管理者ログアウト' do
+    it 'ログイン済みの管理者は、ログアウトできる' do
+      login_as admin, scope: :admin
+      visit admins_root_path
+      expect(page).to have_selector 'h2', text: 'ダッシュボード'
+
+      find('.dropdown-toggle', text: '管理者').click
+      click_button 'ログアウト'
+
+      expect(page).to have_current_path new_admin_session_path
+      expect(page).to have_selector 'h2', text: '管理者ログイン'
+    end
+  end
+end
