d5f12fe5ede90a990c1cb30942c2445bacf7a97b
KeeeeSB <kfp.a@icloud.com>
2025-11-23 08:01:24 +0900

    ciの修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,28 +1,5 @@
-driver = if ENV['CI']
-           Capybara.register_driver :headless_chrome_ci do |app|
-             Capybara::Selenium::Driver.new(
-               app,
-               browser: :chrome,
-               capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-                 "goog:chromeOptions" => {
-                   "args" => %w[
-            headless
-            no-sandbox
-            disable-dev-shm-usage
-            disable-gpu
-            window-size=1400,1400
-          ]
-                 }
-               )
-             )
-           end
-           :headless_chrome_ci
-         else
-           :selenium_chrome_headless
-         end
-
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by driver
+    driven_by :selenium_chrome_headless
   end
 end
