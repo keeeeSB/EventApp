b43952c630891bea45632643cc9673cabb1c6772
KeeeeSB <kfp.a@icloud.com>
2025-11-24 07:31:43 +0900

    バリデーションの追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
   validates :comment, presence: true, length: { maximum: 100 }
   validates :user_id, uniqueness: { scope: :event_id }
 end
