68fa503bc85ebbd66eb81f0a7a2bb3b906990f97
KeeeeSB <kfp.a@icloud.com>
2025-11-29 13:28:10 +0900

    rails_helperの設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,3 +79,15 @@ RSpec.configure do |config|
   # Include FactoryBot syntax method
   config.include FactoryBot::Syntax::Methods
 end
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--window-size=1920,1080')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless
